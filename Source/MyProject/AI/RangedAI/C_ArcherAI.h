e1d8cd0abd8fb88c49b3bb2d180287d007f7d159
IbbyHussain <58857365+IbbyHussain@users.noreply.github.com>
2021-02-11 20:22:51 +0000

    Archer
    set up

---
+++ Source/MyProject/AI/RangedAI/C_ArcherAI.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "MyProject/AI/C_BaseCustomisedAI.h"
+#include "C_ArcherAI.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYPROJECT_API AC_ArcherAI : public AC_BaseCustomisedAI
+{
+	GENERATED_BODY()
+
+	AC_ArcherAI();
+};
