aa7bab6f79635c80e917e536d0c68f9372643c76
IbbyHussain <58857365+IbbyHussain@users.noreply.github.com>
2021-02-16 15:27:26 +0000

    Lost Adventurer AI
    basic setup

---
+++ Source/MyProject/AI/Creatures/C_LostAdventurer.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "MyProject/AI/MeleeAI/C_MeleeAI.h"
+#include "C_LostAdventurer.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYPROJECT_API AC_LostAdventurer : public AC_MeleeAI
+{
+	GENERATED_BODY()
+
+	AC_LostAdventurer();
+	
+};
