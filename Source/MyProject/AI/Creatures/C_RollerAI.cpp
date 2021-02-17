259cbd7007fcdda75e92e65d6594f6a70364d480
IbbyHussain <58857365+IbbyHussain@users.noreply.github.com>
2021-02-17 13:43:24 +0000

    Roller AI
    created class

---
+++ Source/MyProject/AI/Creatures/C_RollerAI.cpp
@@ -0,0 +1,24 @@
+
+#include "C_RollerAI.h"
+
+AC_RollerAI::AC_RollerAI()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	MeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh Comp"));
+	RootComponent = MeshComp;
+
+}
+
+void AC_RollerAI::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void AC_RollerAI::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
---
+++ Source/MyProject/AI/Creatures/C_RollerAI.h
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "C_RollerAI.generated.h"
+
+UCLASS()
+class MYPROJECT_API AC_RollerAI : public APawn
+{
+	GENERATED_BODY()
+
+public:
+
+	AC_RollerAI();
+
+	virtual void Tick(float DeltaTime) override;
+
+protected:
+
+	virtual void BeginPlay() override;
+
+private:
+
+	UPROPERTY(EditDefaultsOnly, Category = "Mesh")
+	UStaticMeshComponent* MeshComp;
+
+
+};
