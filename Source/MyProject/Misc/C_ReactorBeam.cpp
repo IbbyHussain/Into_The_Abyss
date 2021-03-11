936975c56314fbbd04e0aa034627437fd849b57d
IbbyHussain <58857365+IbbyHussain@users.noreply.github.com>
2021-03-11 20:46:07 +0000

    Lightning
    c++ changes

---
+++ Source/MyProject/Misc/C_ReactorBeam.cpp
@@ -0,0 +1,40 @@
+
+#include "C_ReactorBeam.h"
+#include "NiagaraComponent.h"
+#include "Kismet/KismetMathLibrary.h"
+#include "NiagaraSystem.h"
+
+#include "NiagaraFunctionLibrary.h"
+
+AC_ReactorBeam::AC_ReactorBeam()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	BeamComp = CreateDefaultSubobject<UNiagaraComponent>(TEXT("Beam"));
+
+	MaxBeamRange = 500.0f;
+
+	MinBeamRange = -500.0f;
+}
+
+void AC_ReactorBeam::BeginPlay()
+{
+	Super::BeginPlay();
+
+	GetWorldTimerManager().SetTimer(ChangeTargetLocationHandle, this, &AC_ReactorBeam::BecomeCorrupted, 0.5f, true);
+	
+}
+
+void AC_ReactorBeam::BecomeCorrupted()
+{
+	BeamComp->SetNiagaraVariableVec3(FString("Target"), FVector(UKismetMathLibrary::RandomFloatInRange(MinBeamRange, MaxBeamRange), UKismetMathLibrary::RandomFloatInRange(MinBeamRange, MaxBeamRange),
+		UKismetMathLibrary::RandomFloatInRange(MinBeamRange, MaxBeamRange)));
+}
+
+void AC_ReactorBeam::SpawnEffects()
+{
+
+	BeamComp->SetNiagaraVariableVec3(FString("TangentStart"), );
+
+	UNiagaraFunctionLibrary::SpawnSystemAtLocation(GetWorld(), SparksEffect, )
+}
---
+++ Source/MyProject/Misc/C_ReactorBeam.h
@@ -0,0 +1,51 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "C_ReactorBeam.generated.h"
+
+UCLASS()
+class MYPROJECT_API AC_ReactorBeam : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	AC_ReactorBeam();
+
+protected:
+
+	virtual void BeginPlay() override;
+
+	void BecomeCorrupted();
+
+	UPROPERTY(EditDefaultsOnly, Category = "Beam")
+		class UNiagaraComponent* BeamComp;
+
+	// Exposed Variables
+
+	// The location of where the beam will strike 
+	UPROPERTY(EditAnywhere, Category = "Beam")
+	FVector BeamTargetLocation;
+
+	// The max area the beam can hit
+	UPROPERTY(EditAnywhere, Category = "Beam")
+	float MaxBeamRange;
+
+	// The min area the beam can hit
+	UPROPERTY(EditAnywhere, Category = "Beam")
+	float MinBeamRange;
+
+	// The time it takes the lightning to hit a new target location
+	UPROPERTY(EditAnywhere, Category = "Beam")
+	float LightningFrequency;
+
+	FTimerHandle ChangeTargetLocationHandle;
+
+	// Beam spawn
+
+	void SpawnEffects();
+
+	class UNiagaraSystem* SparksEffect;
+};
