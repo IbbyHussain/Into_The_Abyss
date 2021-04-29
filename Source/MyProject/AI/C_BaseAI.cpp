0b19abe0c8d37da759321b0fb9c9c804a6cb2f07
IbbyHussain <58857365+IbbyHussain@users.noreply.github.com>
2021-04-29 19:34:56 +0100

    Lvl 5
    paced all assets

--- Source/MyProject/AI/C_BaseAI.cpp
+++ Source/MyProject/AI/C_BaseAI.cpp
@@ -209,8 +209,11 @@ void AC_BaseAI::Tick(float DeltaTime)
 	if(PlayerCharacter && PlayerCharacter->bDead)
 	{
 		auto const AIController = Cast<AAIController>(UAIBlueprintHelperLibrary::GetAIController(this));
-		UBrainComponent* LocalComp = AIController->GetBrainComponent();
-		LocalComp->StopLogic(FString("Player Death"));
+		if(AIController)
+		{
+			UBrainComponent* LocalComp = AIController->GetBrainComponent();
+			LocalComp->StopLogic(FString("Player Death"));
+		}
 	}
 }
 
